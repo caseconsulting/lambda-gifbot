05fdb9cc45a7b212406d0d022cbaee4ccd922182
Jacob <jacobboisseau@gmail.com>
2018-08-15 12:43:06 -0400

    switched to translate added company check

--- index.js
+++ index.js
@@ -6,30 +6,25 @@ let axios = require('axios');
 exports.handler = function(event, context, callback) {
     let body = JSON.parse(event.body);
     let searchTerm = JSON.stringify(body.command);
-
+    let companyId = body.creator.company.id;
     const apiKey= `${process.env.GifbotAPIDevKey}`;
+    const limit = '50';
     let searchEndPoint = 'https://api.giphy.com/v1/gifs/translate';
 
-  let url = `${searchEndPoint}?api_key=${apiKey}&q=${searchTerm}&limit=500&rating=pg-13`;
+  let url = `${searchEndPoint}?api_key=${apiKey}&s=${searchTerm}&limit=${limit}&rating=pg-13`;
+  if(companyId == process.env.companyId){
 var response;
   axios.get(url)
     .then((response) => {
-      let searchReturn = [];
-      let i;
-      for(i = 0; i < 20; i++)
-      {
-        let random = Math.floor((Math.random() * (response.data.data.length-0) + 0));
-        searchReturn.push(response.data.data[random].images.original.url);
-      }
-      let randomGifUrl = searchReturn[Math.floor((Math.random() * (searchReturn.length-0) + 0))];
+      let gif = response.data.data.images.original.url;
        response = {
         statusCode: 200,
         headers: {
             "x-custom-header" : "my custom header value"
         },
-        body: randomGifUrl
+        body: gif
     };
-    console.log('sending this gif',randomGifUrl);
+    console.log('sending this gif',gif);
     callback(null, response);
     })
    .catch((error) => {
@@ -43,4 +38,15 @@ var response;
    console.log("something went wrong", error);
    callback(null, response);
     });
+  }else{
+      response = {
+       statusCode: 403,
+       headers: {
+           "x-custom-header" : "my custom header value"
+       },
+       body: "Access Denied"
+   };
+   console.log("access denied");
+   callback(null, response);
+  }
 };
